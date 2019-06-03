9a5b04a556c59d0ae1677f5052cc250f664d54f1
pvl1175 <vitaly.popov.75@gmail.com>
2019-06-03 16:02:31 +0300

    update

---
+++ lookfor9.thrift
@@ -0,0 +1,21 @@
+namespace cpp lookfor9.api
+namespace java lookfor9.api
+namespace php lookfor9.api
+namespace netcore lookfor9.api
+namespace netcore lookfor9.api
+namespace js lookfor9.api
+
+struct ProxyInfo
+{
+	1: string Url,
+	2: string State,
+	3: i32 TimeAlive,
+	4: string LastCheck;
+}
+
+service Api
+{
+	string Hello()
+	void AddProxy(1: string Url)
+	string PhoneInfo(1: string number)
+}
